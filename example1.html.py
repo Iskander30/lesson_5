3ddd1b9a65e6525f50d22ddb9f836bc60d967329
Iskander <example@mail.ru>
2023-06-29 18:15:07 +0300

    ver1

---
+++ example1.html.py
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Page1</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+</head>
+<body>
+<a href="page2.html" target="blank">Ссылка на страницу 2</a>
+<p class="lead">
+    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Alias at deleniti eos error ipsum laboriosam mollitia nisi
+nobis nulla obcaecati, quia quidem quisquam quo quod suscipit velit voluptatum. Consectetur, molestiae?
+</p>
+<form action="">
+    <label for="field_1" class="lead">Введите ваше имя:</label>
+    <input type="text" id="field_1" class="form-control form-control-lg"><br>
+    <label for="field_2">Чек-бокс:</label>
+    <input type="checkbox" id="field_2 class="lead"" class="form-check-input" ><br>
+    <label for="field_3" class="lead">Введите ваше пароль:</label>
+    <input type="password" id="field_3" class="form-control form-control-sm"><br>
+    <input type="button" value="Отправить форму" class="btn btn-warning"><br>
+    <textarea name="text" cols="60" rows="3" placeholder="Напишите ваши впечатления о сайте"></textarea
+</form>
